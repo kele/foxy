5d5ab3245be2035a923c4bdcd61ca0291065aa1c
kele <damian.bogel@gmail.com>
2017-06-05 11:59:06 +0200

    main.rs draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net;
+
+const PROXY_PORT: u16 = 4000;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = net::TcpListener::bind(("127.0.0.1", PROXY_PORT)).unwrap();
+
+    match listener.accept() {
+        Ok((sock, _)) => handle_connection(sock),
+        Err(e) => panic!("Error while accepting connection: {}", e),
+    }
+}
+
+fn handle_connection(tcp: net::TcpStream) {
+    println!("Opened connection: {:?}", tcp)
 }
